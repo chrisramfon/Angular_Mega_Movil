31e77aae072f48695acce75ccb8cf6f940375f19
AldoxContreras <68927132+AldoxContreras@users.noreply.github.com>
2020-11-13 00:10:35 -0600

    Correción
    correcion en los imports de la cita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,34 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+
 //Componentes
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 //Servicios
 import { InicioSesionService } from './servicios/inicio-sesion.service';
-import { ClienteCitaComponent } from './cliente-cita/cliente-cita.component';
+import { ClienteCitaService } from './servicios/cliente-cita.service';
 
 @NgModule({
+
   declarations: [
     AppComponent,
     InicioSesionComponent,
     ClienteCitaComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
+
   providers: [
-    InicioSesionService
+    InicioSesionService,
+    ClienteCitaService,
   ],
+
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
