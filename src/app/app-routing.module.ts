6e118196ecdf94a11b5ad57d7e16dd5a8d9fe735
AldoxContreras <68927132+AldoxContreras@users.noreply.github.com>
2020-11-13 00:01:46 -0600

    Cita
    Le aventajé o algunos codigos, me pidió instalar el tslib, peero al parecer se actualizó, aún no está completo el de citas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //Componentes
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { ClienteCitaComponent } from './cliente-cita/cliente-cita.component';
+
 
 const routes: Routes = [
   {path:'inicio', component:InicioSesionComponent},
+  {path:'cita', component:ClienteCitaComponent},
+
+
+
   //Mantener estas dos lineas al final
   {path:'',redirectTo:'inicio', pathMatch:'full'},
-  {path:'**', redirectTo:'inicio',pathMatch:'full'}
+  {path:'**', redirectTo:'inicio',pathMatch:'full'},
+  {path:'',redirectTo:'cita',pathMatch:'full'},
+  {path:'**', redirectTo:'cita',pathMatch:'full'},
 ];
 
 @NgModule({
--- src/app/servicios/cliente-cita.service.ts
+++ src/app/servicios/cliente-cita.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteCitaService {
 
-  constructor() { }
+
+
+export class ClienteCitaService {
+  url = "http://localhost:3000/clientes/nuevacita";
+  @Output() change: EventEmitter<boolean> = new EventEmitter();
+  @Output() change2: EventEmitter<String> = new EventEmitter();
+  
+
+  constructor(private http: HttpClient){}
+
+
+//Manda los datos a la ruta de inicio de sesion de express
+  cita(cliente){
+    return this.http.post<any>(this.url,cliente);
+  }
+
+
 }
